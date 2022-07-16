b09fbdf2c93d72d4c16f7c51a542e133c72b58f6
sukarus0 <sukarus@gamil.com>
2022-07-16 14:22:55 +0000

    Add BigDataComponent Model

--- master/models.py
+++ master/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class BigDataComponent(models.Model):
+    category = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
