af6d2628566fc5db94778e9c376da745405e089e
sukarus0 <sukarus@gamil.com>
2022-07-20 02:51:24 +0000

    Add start,stop,restart API

--- component_control/urls.py
+++ component_control/urls.py
@@ -1,7 +1,14 @@
+from django.urls import path
 from rest_framework import routers
-from .views import ComponentStatusViewSet
+from .views import ComponentStatusViewSet, startComponent, stopComponent, restartComponent
 
 router = routers.SimpleRouter()
 router.register('info', ComponentStatusViewSet)
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('start/', startComponent),
+    path('stop/', stopComponent),
+    path('restart/', restartComponent),
+]
--- component_control/views.py
+++ component_control/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import ComponentStatus
 from .serializers import ComponentStatusSerializer
@@ -7,3 +9,25 @@ from .serializers import ComponentStatusSerializer
 class ComponentStatusViewSet(viewsets.ModelViewSet):
     queryset = ComponentStatus.objects.all()
     serializer_class = ComponentStatusSerializer
+
+
+@api_view(['GET', 'POST'])
+def startComponent(request):
+    if request.method == 'GET':
+        return Response("Component Started.")
+    elif request.method == 'POST':
+        return Response(f"Component Started with options : {request.data}")
+
+@api_view(['GET', 'POST'])
+def stopComponent(request):
+    if request.method == 'GET':
+        return Response("Component Stopped.")
+    elif request.method == 'POST':
+        return Response(f"Component Stopped with options : {request.data}")
+
+@api_view(['GET', 'POST'])
+def restartComponent(request):
+    if request.method == 'GET':
+        return Response("Component Restarted.")
+    elif request.method == 'POST':
+        return Response(f"Component Restarted with options : {request.data}")
