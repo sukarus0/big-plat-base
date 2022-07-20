5a83b6199758b4f23b126561c1f2ff8456764927
sukarus0 <sukarus@gamil.com>
2022-07-20 02:12:11 +0000

    Add testcode

--- component_control/tests.py
+++ component_control/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+# from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.views import status
 
 # Create your tests here.
+class componentControlTestCase(APITestCase):
+
+    def setUp(self):
+        self.client.post('/component-control/info/', {'name': 'kafka', 'status': 'running'}, format='json')
+        self.client.post('/component-control/info/', {'name': 'hdfs', 'status': 'running'}, format='json')
+        self.client.post('/component-control/info/', {'name': 'spark', 'status': 'pending'}, format='json')
+
+    def test_get_component_control_info(self):
+        response = self.client.get('/component-control/info/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'kafka')
+        self.assertEqual(response.data['status'], 'running')
+
+        response = self.client.get('/component-control/info/2/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'hdfs')
+        self.assertEqual(response.data['status'], 'running')
+        
+        response = self.client.get('/component-control/info/3/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'spark')
+        self.assertEqual(response.data['status'], 'pending')
